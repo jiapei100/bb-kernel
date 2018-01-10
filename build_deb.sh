ec18e670f7620d014c4d7748da992328761c57af
jiapei100 <jp4work@gmail.com>
2018-01-10 16:03:01 +0800

    build_deb.sh build_kernel.sh both removed gcc.sh and git.sh .

--- build_deb.sh
+++ build_deb.sh
@@ -144,8 +144,8 @@ unset LINUX_GIT
 if [  -f "${DIR}/.yakbuild" ] ; then
 	. "${DIR}/recipe.sh"
 fi
-/bin/sh -e "${DIR}/scripts/gcc.sh" || { exit 1 ; }
-. "${DIR}/.CC"
+#/bin/sh -e "${DIR}/scripts/gcc.sh" || { exit 1 ; }
+#. "${DIR}/.CC"
 echo "CROSS_COMPILE=${CC}"
 if [ -f /usr/bin/ccache ] ; then
 	echo "ccache [enabled]"
@@ -154,6 +154,7 @@ fi
 
 . "${DIR}/version.sh"
 export LINUX_GIT
+echo "LINUX_GIT=${LINUX_GIT}"
 
 if [ ! "${CORES}" ] ; then
 	CORES=$(getconf _NPROCESSORS_ONLN)
@@ -162,7 +163,7 @@ fi
 #unset FULL_REBUILD
 FULL_REBUILD=1
 if [ "${FULL_REBUILD}" ] ; then
-	/bin/sh -e "${DIR}/scripts/git.sh" || { exit 1 ; }
+	#/bin/sh -e "${DIR}/scripts/git.sh" || { exit 1 ; }
 
 	if [ "${RUN_BISECT}" ] ; then
 		/bin/sh -e "${DIR}/scripts/bisect.sh" || { exit 1 ; }
