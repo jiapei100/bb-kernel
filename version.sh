c998e98ec8138114b6a3924c5f46b3a7bd6a28ae
jiapei100 <jp4work@gmail.com>
2018-01-10 13:38:51 +0800

    toolchain=/usr/bin/arm-linux-gnueabihf-gcc

--- version.sh
+++ version.sh
@@ -21,7 +21,7 @@ KERNEL_ARCH=arm
 #toolchain="gcc_linaro_gnueabihf_4_9"
 #toolchain="gcc_linaro_gnueabihf_5"
 #toolchain="gcc_linaro_gnueabihf_6"
-toolchain="gcc_linaro_gnueabihf_7"
+toolchain="/usr/bin/arm-linux-gnueabihf-gcc"
 #arm64
 #KERNEL_ARCH=arm64
 #toolchain="gcc_linaro_aarch64_gnu_5"
